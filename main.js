6b6a1f35056423adb9c959f48b4f80641234eb54
Vlad Romanovich <romanovich.vo@gmail.com>
2017-04-19 16:21:46 +0300

    Change pm2 config

--- main.js
+++ main.js
@@ -17,5 +17,18 @@ pm2.connect(function() {
   }, function(err) {
     if (err) return console.error('Error while launching applications', err.stack || err);
     console.log('PM2 and application has been succesfully started');
+
+    // Display logs in standard output
+    pm2.launchBus(function(err, bus) {
+      console.log('[PM2] Log streaming started');
+
+      bus.on('log:out', function(packet) {
+        console.log('[App:%s] %s', packet.process.name, packet.data);
+      });
+
+      bus.on('log:err', function(packet) {
+        console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+      });
+    });
   });
 });
