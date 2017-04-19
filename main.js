184dbc28d1a19c24750b0895186af8d5aabbaa0d
Vlad Romanovich <romanovich.vo@gmail.com>
2017-04-19 16:59:47 +0300

    Change pm2 file

--- main.js
+++ main.js
@@ -12,23 +12,10 @@ pm2.connect(function() {
     max_memory_restart : maxMemory + 'M',   // Auto-restart if process takes more than XXmo
     env: {                            // If needed declare some environment variables
       "NODE_ENV": "production",
-
+      "PORT": 8080
     },
   }, function(err) {
     if (err) return console.error('Error while launching applications', err.stack || err);
     console.log('PM2 and application has been succesfully started');
-
-    // Display logs in standard output
-    pm2.launchBus(function(err, bus) {
-      console.log('[PM2] Log streaming started');
-
-      bus.on('log:out', function(packet) {
-        console.log('[App:%s] %s', packet.process.name, packet.data);
-      });
-
-      bus.on('log:err', function(packet) {
-        console.error('[App:%s][Err] %s', packet.process.name, packet.data);
-      });
-    });
   });
 });
