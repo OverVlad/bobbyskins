91e0c0f36adb15fc1b7a6b07751793cc736ce52b
Vlad Romanovich <romanovich.vo@gmail.com>
2017-04-19 14:47:31 +0300

    change shipitfiles pm2

--- shipitfile.js
+++ shipitfile.js
@@ -5,6 +5,7 @@ module.exports = function (shipit) {
     shipit.on('deployed', () => {
         shipit.remote(`cd ${shipit.currentPath} && npm i`)
         .then(() => shipit.remote(`cd ${shipit.config.deployTo}/shared && cp ./server_config.js ${shipit.currentPath}/server/config.js`))
+        .then(() => shipit.remote('pm2 stop main'))
         .then(() => shipit.remote(`cd ${shipit.currentPath} && pm2 start main.js`))
     });
 
