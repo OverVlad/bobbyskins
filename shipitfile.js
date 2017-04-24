b48330998c246a37927dc0482ea1c13e7a8f2bee
Vlad Romanovich <romanovich.vo@gmail.com>
2017-04-24 12:18:13 +0300

    Add roulette on server

--- shipitfile.js
+++ shipitfile.js
@@ -6,7 +6,7 @@ module.exports = function (shipit) {
         shipit.remote(`cd ${shipit.currentPath} && npm i`)
         .then(() => shipit.remote(`cd ${shipit.config.deployTo}/shared && cp ./server_config.js ${shipit.currentPath}/server/config.js`))
         .then(() => shipit.remote('pm2 kill'))
-        .then(() => shipit.remote(`cd ${shipit.currentPath} && pm2 start main.js`))
+        .then(() => shipit.remote(`cd ${shipit.currentPath} && npm run prod`))
     });
 
     shipit.initConfig({
