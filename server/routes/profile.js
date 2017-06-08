e50849d2fd1b222b95d5357b4c1a947b1a6ec7ed
Vlad Romanovich <romanovich.vo@gmail.com>
2017-06-08 21:03:20 +0300

    Empty route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -12,4 +12,16 @@ router.get('/:id/poker-stats', function (req, res, next) {
   PokerBet.getStat(req, res);
 });
 
+router.get('/:id/trade-history', function (req, res, next) {
+  res.sentStatus(200);
+});
+
+router.get('/:id/referals', function (req, res, next) {
+  res.sentStatus(200);
+});
+
+router.get('/:id/common-info', function (req, res, next) {
+  res.sentStatus(200);
+});
+
 module.exports = router;
