ef5aaa2d4388c6b65899ae9ac1c9481e915ba5d9
Andrei Sheina <andrew.sheina@gmail.com>
2017-06-08 11:36:18 +0300

    add react-table

--- client/src/components/Profile/ProfileRouletteStats.jsx
+++ client/src/components/Profile/ProfileRouletteStats.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import ReactTable from 'react-table'
 import { fetchRouletteStats } from '../../actions/profileActions';
 
 class ProfileRouletteStats extends Component {
@@ -9,6 +10,76 @@ class ProfileRouletteStats extends Component {
   // }
 
   render() {
+    const data = [{
+      betId: '123456',
+      round: '12345',
+      time: '14:45:12',
+      betAmount: '13500',
+      bet: 'Нечетные',
+      roll: '10',
+      profit: '10000',
+    },{
+      betId: '123456',
+      round: '12345',
+      time: '14:45:12',
+      betAmount: '13500',
+      bet: 'Нечетные',
+      roll: '10',
+      profit: '55500',
+    },{
+      betId: '123456',
+      round: '12345',
+      time: '14:45:12',
+      betAmount: '13500',
+      bet: 'Нечетные',
+      roll: '10',
+      profit: '55500',
+    },{
+      betId: '123456',
+      round: '12345',
+      time: '14:45:12',
+      betAmount: '13500',
+      bet: 'Нечетные',
+      roll: '10',
+      profit: '55500',
+    },{
+      betId: '123456',
+      round: '12345',
+      time: '14:45:12',
+      betAmount: '13500',
+      bet: 'Нечетные',
+      roll: '10',
+      profit: '55500',
+    }];
+
+    const columns = [{
+      Header: 'Номер ставки',
+      accessor: 'betId'
+    },
+    {
+      Header: 'Номер раунда',
+      accessor: 'round'
+    },
+    {
+      Header: 'Время',
+      accessor: 'time'
+    },
+    {
+      Header: 'Размер ставки',
+      accessor: 'betAmount'
+    },
+    {
+      Header: 'Ставка на',
+      accessor: 'bet'
+    },
+    {
+      Header: 'Выпало',
+      accessor: 'roll'
+    },{
+      Header: 'Прибыль',
+      accessor: 'profit'
+    }];
+
     return (
       <div className="profile-data-wrapper">
         <h2>История ставок в рулетке</h2>
@@ -48,7 +119,10 @@ class ProfileRouletteStats extends Component {
         </div>
 
         <div className="wrapper">
-          Content for roulette-stats
+          <ReactTable
+            data={data}
+            columns={columns}
+          />
         </div>
       </div>
     );
